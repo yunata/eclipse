cf21cc9d484b2d525f766b79ea86abe76744ceb1
yunata <hanachirusato54@gmail.com>
2018-07-24 10:43:18 +0900

    test2merge

--- src/test/java/test/Test2Test.java
+++ src/test/java/test/Test2Test.java
@@ -1,15 +1,13 @@
 package test;
 
-import java.util.Date;
-
 import org.junit.Test;
+
 
 public class Test2Test {
 
 	@Test
-	public void 日付の比較() throws Exception {
-		Date date = new Date();
-		assertthat(date,is(dateOf("2011,2,10")));
+	public void myFirstTest() {
+		assertEquals(2,1+1);
 
 	}
 
